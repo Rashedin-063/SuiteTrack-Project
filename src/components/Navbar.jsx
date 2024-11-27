101471c127f7ac9e29f2e4777e1c5935f37d45a3
HabibFahim15 <habibfahim71@gmail.com>
2024-11-27 17:34:35 +0600

    navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,17 @@
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material"
 const Navbar = () => {
   return (
-    <header className="p-4 dark:bg-gray-100 dark:text-gray-800">
-      <div className="container flex justify-between h-16 mx-auto">
-        <a rel="noopener noreferrer" href="#" aria-label="Back to homepage" className="flex items-center p-2">
-          <h2>SuiteTrack</h2>
-        </a>
-        <ul className="items-stretch hidden space-x-3 lg:flex">
-          <li className="flex">
-            <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border- dark:text-violet-600 dark:border-violet-600">Link</a>
-          </li>
-          <li className="flex">
-            <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-">Home</a>
-          </li>
-          <li className="flex">
-            <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-">About</a>
-          </li>
-          <li className="flex">
-            <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-">Blog</a>
-          </li>
-          <li className="flex">
-            <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-">Contact</a>
-          </li>
-        </ul>
-        <div className="items-center flex-shrink-0 hidden lg:flex">
-          <button className="self-center px-8 py-3 rounded">Sign in</button>
-          <button className="self-center px-8 py-3 font-semibold rounded dark:bg-violet-600 dark:text-gray-50">Sign up</button>
-        </div>
-        <button className="p-4 lg:hidden">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 dark:text-gray-800">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-          </svg>
-        </button>
-      </div>
-    </header>
+   <AppBar position='static'>
+    <Toolbar>
+      <IconButton size='large' edge='start' color='inherit' aria-label='logo' >
+        {/* Logo Here */}
+      </IconButton>
+      <Typography variant='h6' component='div' >
+        {/* Title Here */}
+        SuiteTrack
+      </Typography>
+    </Toolbar>
+   </AppBar>
   )
 }
 export default Navbar
