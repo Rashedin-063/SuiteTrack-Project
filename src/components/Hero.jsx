36d043f0f604104da2daee29f21a9f41a79a0986
5anne <tahaminaanne@gmail.com>
2024-11-27 14:33:46 +0600

    hero_corrected

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,13 +9,13 @@ const Hero = () => {
       <div className="bg-no-repeat bg-cover bg-center bg-[#30333866] bg-blend-multiply rounded-3xl p-32" style={{ backgroundImage: "url(https://i.postimg.cc/bN3Vp9XS/img2.jpg)" }}>
         <h6 className="text-center text-7xl font-thin max-w-4xl mx-auto">Where every journey become an adventure</h6>
         <form className="flex justify-center relative py-10" action="">
-          <p className="absolute top-14 left-[190px] text-black"><FaMapMarkerAlt /></p>
+          <p className="absolute top-14 left-[140px] text-black"><FaMapMarkerAlt /></p>
           <input type="text" placeholder="Where you want to go?..." className="rounded-l-3xl text-black py-3 pl-8" />
-          <p className="absolute top-14 left-[415px] text-black"><MdOutlineDateRange /></p>
+          <p className="absolute top-14 left-[365px] text-black"><MdOutlineDateRange /></p>
           <input type="text" placeholder="Add dates" className="text-black py-3 pl-8" />
-          <p className="absolute top-14 left-[640px] text-black"><IoBed /></p>
+          <p className="absolute top-14 left-[590px] text-black"><IoBed /></p>
           <input type="text" placeholder="Guests" className="rounded-r-3xl text-black py-3 pl-8" />
-          <button className="absolute right-[200px] top-11 bg-black rounded-3xl flex items-center gap-2 p-2">Search <FaArrowRightLong /></button>
+          <button className="absolute right-[150px] top-11 bg-black rounded-3xl flex items-center gap-2 p-2">Search <FaArrowRightLong /></button>
         </form>
       </div>
       <div className="flex gap-8 absolute top-[450px] left-12">
