81e49ad2af1af43e3a2e165aa0d156795a272715
afrin823 <afrin12490@gmail.com>
2024-11-27 15:56:21 +0600

    review

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,17 @@
 import Hero from "@/components/Hero";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import {Review} from "@/components/Review"
 import Services from "@/components/Services";
-
+import Faq from "@/components/faq/Faq";
 export default function Home() {
   return (
-    <div className="bg-indigo-900 text-white space-y-16 lg:space-y-28">
+    <div className="bg-[#E2E1DF] text-white space-y-16 lg:space-y-28">
       <Navbar/>
       <Hero />
       <Services />
+     <Review />
+      <Faq />
       <Footer />
    </div>
  )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,45 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+ 
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 module.exports = {
   content: [
     './src/pages/**/*.{js,jsx,mdx}',
     './src/components/**/*.{js,jsx,mdx}',
     './src/app/**/*.{js,jsx,mdx}',
   ],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
       },
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+      },
+      animation: {
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
