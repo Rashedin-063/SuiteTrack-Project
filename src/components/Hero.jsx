5f6ef537bef96b4e6f5bc7341b9adc11dbeb019e
5anne <tahaminaanne@gmail.com>
2024-11-27 14:25:01 +0600

    hero_updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
         </div>
         <div>
           <h6 className="text-3xl">20,000+</h6>
-          <p>Happy Customer</p>
+          <p>Happy Customers</p>
         </div>
         <div>
           <h6 className="text-3xl">100+</h6>
