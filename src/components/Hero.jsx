e348ed53025352b3f4c6106329791b24b997273d
5anne <tahaminaanne@gmail.com>
2024-11-30 18:44:48 +0600

    hero_updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,38 +1,35 @@
-import { FaMapMarkerAlt } from "react-icons/fa"
 import { FaArrowRightLong } from "react-icons/fa6"
-import { IoBed } from "react-icons/io5"
-import { MdOutlineDateRange } from "react-icons/md"
 
 const Hero = () => {
   return (
-    <div className="relative">
-      <div className="bg-no-repeat bg-cover bg-center bg-[#30333866] bg-blend-multiply rounded-3xl p-32" style={{ backgroundImage: "url(https://i.postimg.cc/bN3Vp9XS/img2.jpg)" }}>
-        <h6 className="text-center text-7xl font-thin max-w-4xl mx-auto">Where every journey become an adventure</h6>
-        <form className="flex justify-center relative py-10" action="">
-          <p className="absolute top-14 left-[140px] text-black"><FaMapMarkerAlt /></p>
-          <input type="text" placeholder="Where you want to go?..." className="rounded-l-3xl text-black py-3 pl-8" />
-          <p className="absolute top-14 left-[365px] text-black"><MdOutlineDateRange /></p>
-          <input type="text" placeholder="Add dates" className="text-black py-3 pl-8" />
-          <p className="absolute top-14 left-[590px] text-black"><IoBed /></p>
-          <input type="text" placeholder="Guests" className="rounded-r-3xl text-black py-3 pl-8" />
-          <button className="absolute right-[150px] top-11 bg-black rounded-3xl flex items-center gap-2 p-2">Search <FaArrowRightLong /></button>
-        </form>
+    <div>
+      <div className="bg-no-repeat bg-cover bg-center rounded-3xl px-32 pt-32" style={{ backgroundImage: "url(https://i.postimg.cc/9X9JZgWH/interior-design-neoclassical-style-with-furnishings-decor-1.jpg)" }}>
+        <h6 className="text-center text-5xl text-white font-thin max-w-3xl mx-auto uppercase">Indulge Your Wanderlust: Book Your Stay</h6>
+        <div className="bg-[#00000099] m-10 p-4">
+          <form className="flex justify-center border-2 border-[#bd9966] rounded-tl-3xl rounded-br-3xl relative p-8 m-10" action="">
+            <input type="text" defaultValue="Where you want to go?..." className="rounded-l-3xl bg-[#f9fafb80] border-x-2 text-black py-3 pl-8" />
+            <input type="text" defaultValue="Add dates" className="text-black bg-[#f9fafb80] py-3 pl-8" />
+            <input type="text" defaultValue="Guests" className="rounded-r-3xl bg-[#f9fafb80] border-x-2 text-black py-3 pl-8" />
+            <button className="absolute right-[50px] top-1/3 bg-[#bd9966] hover:bg-[#bd996680] text-white rounded-3xl flex items-center gap-2 p-2">Search <FaArrowRightLong /></button>
+          </form>
+        </div>
+        <div className="flex gap-8 text-white mt-32 py-8 px-4 bg-[#00000099] w-1/2">
+          <div>
+            <h6 className="text-3xl">300+</h6>
+            <p>Destination</p>
+          </div>
+          <div>
+            <h6 className="text-3xl">20,000+</h6>
+            <p>Happy Customer</p>
+          </div>
+          <div>
+            <h6 className="text-3xl">100+</h6>
+            <p>Experienced Guide</p>
+          </div>
+        </div>
       </div>
-      <div className="flex gap-8 absolute top-[450px] left-12">
-        <div>
-          <h6 className="text-3xl">300+</h6>
-          <p>Destination</p>
-        </div>
-        <div>
-          <h6 className="text-3xl">20,000+</h6>
-          <p>Happy Customer</p>
-        </div>
-        <div>
-          <h6 className="text-3xl">100+</h6>
-          <p>Experienced Guide</p>
-        </div>
-      </div>
+
     </div>
   )
 }
-export default Hero
+export default Hero;
