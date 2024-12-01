f127db8920f334e6679674a9fbc1d24041bc578f
5anne <tahaminaanne@gmail.com>
2024-12-01 19:06:27 +0600

    faq_updated

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,37 +1,74 @@
 "use client"
 
 import React from 'react';
-import dynamic from 'next/dynamic'; //
-// import Lottie from 'lottie-react';
-const Lottie = dynamic(() => import('lottie-react'), { ssr: false });
-import faq from "@/data/faq"
+import SectionTitle from './SectionTitle';
+import { StickyScroll } from './ui/sticky-scroll-reveal';
+import Image from 'next/image';
+import service from "../Images/hotel-room-service.jpg"
+import service2 from "../Images/staff-service.avif"
 
 const Faq = () => {
-  return (
-    <div className='max-w-screen-xl text-black mx-auto py-16'>
-   <h1 className='text-center text-4xl font-bold '> Frequently Asked Questions</h1>
-      <div className="grid grid-cols-1 bg-[#FEF9F2] rounded-lg shadow-md md:grid-cols-2 gap-8 items-center p-8">
-      <Lottie animationData={faq} className="w-full h-auto max-w-md" />
-      <div className="space-y-4">
-			<details className="w-full  rounded-lg">
-				<summary className="px-4 py-6 text-gray-800">Ex orci laoreet egestas sapien magna egestas scelerisque?</summary>
-				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-600">Lectus iaculis orci metus vitae ligula dictum per. Nisl per nullam taciti at adipiscing est. </p>
-			</details>
-			<details className="w-full rounded-lg">
-				<summary className="px-4 py-6 focus:outline-none text-gray-800">Lorem at arcu rutrum viverra metus sapien venenatis lobortis odio?</summary>
-				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-600">Tincidunt ut hac condimentum rhoncus phasellus nostra. Magna porttitor egestas tincidunt neque vehicula potenti. </p>
-			</details>
-			<details className="w-full text-gray-800 rounded-lg">
-				<summary className="px-4 py-6 focus:outline-none focus-visible:dark:ring-violet-600">Eleifend feugiat sollicitudin laoreet adipiscing bibendum suscipit erat?</summary>
-				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-800">Justo libero tellus integer tincidunt justo semper consequat venenatis aliquet imperdiet. Ultricies urna proin fusce nulla pretium sodales vel magna et massa euismod vulputate sed. </p>
-			</details>
+	const content = [
+		{
+			title: "What payment methods do you accept?",
+			description:
+				"Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.....",
+			content: (
+				<div className="h-full w-full flex items-center justify-center text-white">
+					<Image width={500} height={500} alt='photo' src={service} />
+				</div>
+			),
+		},
+		{
+			title: "Can I modify or cancel my booking?",
+			description:
+				"See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.....",
+			content: (
+				<div className="h-full w-full flex items-center justify-center text-white">
+					<Image width={500} height={500} alt='photo' src={service2} />
+				</div>
+			),
+		},
+		{
+			title: "What amenities are included in each room?",
+			description:
+				"Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.....",
+			content: (
+				<div className="h-full w-full flex items-center justify-center text-white">
+					<Image width={500} height={500} alt='photo' src={service} />
+				</div>
+			),
+		},
+		{
+			title: "Can you arrange transportation from the airport?",
+			description:
+				"Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.....",
+			content: (
+				<div className="h-full w-full flex items-center justify-center text-white">
+					<Image width={500} height={500} alt='photo' src={service2} />
+				</div>
+			),
+		},
+		{
+			title: "What are the check-in and check-out times?",
+			description:
+				"Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions......",
+			content: (
+				<div className="h-full w-full flex items-center justify-center text-white">
+					<Image width={500} height={500} alt='photo' src={service} />
+				</div>
+			),
+		},
+	];
+	return (
+		<div className='max-w-screen-xl text-black mx-auto py-16'>
+			<SectionTitle heading="Frequently Asked Questions"></SectionTitle>
+			<div className="p-10">
+				<StickyScroll content={content} />
+			</div>
 		</div>
-      </div>
 
-	
-	</div>
-
-  );
+	);
 };
 
 export default Faq;
