c2eac278a021072a88c7f8e58dbbeac211b033c2
Rashedin-063 <rashedinislam.06@gmail.com>
2024-12-02 17:18:36 +0600

    api created

--- src/app/ourRoom/page.jsx
+++ src/app/ourRoom/page.jsx
@@ -1,7 +1,7 @@
 
 const OurRoomPage = async() => {
   const fetchRooms = async () => {
-   const res = await fetch(`${process.env.BASE_URL}/api/rooms`);
+   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/rooms`);
     const roomData = await res.json();
     return roomData
   }
